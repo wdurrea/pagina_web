568a384a46d89f0546b5a60092938d01f7b8edf4
StevenSanchezGarcia <50110822+StevenSanchezGarcia@users.noreply.github.com>
2020-05-12 01:33:17 -0500

    Subiendo por primera ves la pagina

---
+++ DescubreYAprende/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('header label').addEventListener('click',() =>{
+  document.querySelector('.menu-bar').classcontenedor.toggle('show');
+})
+ScrollReveal().reveal('#banner',{delay:200});
+ScrollReveal().reveal('.contenedor',{delay:500});
---
+++ DescubreYAprende/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+     
+    $('#btnSend').click(function(){
+        var errores='';
+
+        if($("#names").val() == '' ){
+            errores +='<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#f14b4b");
+        }else{
+            $('#names').css("border-bottom-color", "#d1d1d1");
+        }
+
+        if($('#email').val() == ''){
+            errores += '<p>Ingrese un correo electronico</p>';
+            $('#email').css("border-bottom-color", "#f14b4b");
+        }else{
+            $('#email').css("border-bottom-color", "#d1d1d1");
+        }
+
+        if($('#mensaje').val() == ''){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#f14b4b");
+        }else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1");
+        }
+
+        
+        if(errores == '' == false){
+            var mensajeModal= '<div class="modal_wrap">'+
+                                '<div class="mensaje_modal">'+
+                                '<h3>Errores encontrados</h3>'+
+                                errores+
+                                '<span id="btnClose">Cerrar</span>'+
+                                '</div>'+
+                              '</div>'
+
+            $('body').append(mensajeModal);               
+        }
+
+       
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        })
+
+    });
+
+    
+});
